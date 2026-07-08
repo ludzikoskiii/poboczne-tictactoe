70806ddd8b413a6bbc814c747db3c402787324a1
Kuba G <kubagorczowski123@gmail.com>
2026-07-08 09:45:24 +0200

    refactor: dodanie komentarzy zwiekszajacych czytelnosc kodu

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// pobierz blok glowny i blok wyników
 const blokGlowny = document.querySelector("#blok-glowny");
 const blokWynik = document.querySelector("#blok-wynik");
 
-        class gracz{
+// klasa gracza
+class gracz{
             constructor(nazwa,wynik,figura,numerGracza){
                 this.nazwa = nazwa;
                 this.wynik = wynik;
@@ -13,6 +15,7 @@ const blokWynik = document.querySelector("#blok-wynik");
             }
         }
 
+//klasa parametry gry
         class parametryGry{
             constructor(aktualnyGracz,wygrana,remis,numerAktualnegoGracza){
                 this.aktualnyGracz = aktualnyGracz;
@@ -22,6 +25,7 @@ const blokWynik = document.querySelector("#blok-wynik");
             }
         }
 
+//klasa parametry kratek
         class parametryKratek{
             constructor(kratka,figura){
                 this.kratka = kratka;
@@ -29,13 +33,14 @@ const blokWynik = document.querySelector("#blok-wynik");
             }
         }
 
+//klasa parametry czasomierza
         class parametryCzasomierza{
             constructor(czasomierz,sekundy){
                 this.czasomierz = czasomierz;
                 this.sekundy = sekundy;
             }
         }
-        
+//rozpoczecie czasomierza       
         function czasomierz(iloscMinut){
             let sekundy = iloscMinut * 60;
             setInterval(() => {
@@ -43,29 +48,31 @@ const blokWynik = document.querySelector("#blok-wynik");
                 sekundy--;
             }, 1000);
         }
-
+//zatrzymanie czasomierza
         function stopCzasomierz(mojCzasomierz){
             clearInterval(mojCzasomierz)
             let sekundy = mojCzasomierz.sekundy;
             document.querySelector("#czasomierz").innerHTML = `Pozostały czas: ${Math.floor(sekundy / 60)}:${sekundy % 60 < 10 ? "0" + sekundy % 60 : sekundy % 60}`;
         }
+//utworzenie instancji klasy gracz
         function utworzGraczy(){
             // const nazwaGracza1 = document.querySelector("#nazwa-gracza-1").value;
             // const nazwaGracza2 = document.querySelector("#nazwa-gracza-2").value;
             gracz1 = new gracz("nazwaGracza1",0,"X",1);
             gracz2 = new gracz("nazwaGracza2",0,"O",2);
         }
+//rozpoczenie czasomierza i ustawienie parametrow gry
         function startujGre() {
             const iloscMinut = document.querySelector("#wybor-czasu").value;
             
             parametryGry = new parametryGry(1,false,false);
             czasomierz(iloscMinut);
         }
-
+//reset czasomierza i reset parametrow gry
         function koniecGry(){
             stopCzasomierz(czasomierz);
         }
-        
+//zmiana aktualnego gracza podczas gry po postawieniu figury       
         function zmianaGracza(){
             if(parametryGry.aktualnyGracz === 1){
                 parametryGry.aktualnyGracz = 2;
@@ -74,7 +81,7 @@ const blokWynik = document.querySelector("#blok-wynik");
                 parametryGry.aktualnyGracz = 1;
             }
         }
-
+//sprawdzenie aktualnej figury gracza
         function sprawdzenieAktualnejFigury(){
              let figura = "";
             if(parametryGry.aktualnyGracz === 1){
@@ -85,7 +92,7 @@ const blokWynik = document.querySelector("#blok-wynik");
             }
             return figura;
         }
-
+//sprawdzenie czy gracz wygrał poprzez sprawdzenie kombinacji
         function sprawdzanieKombinacji(tablica){
                 let kombinacje = [
                     [1, 2, 3],
@@ -104,12 +111,12 @@ const blokWynik = document.querySelector("#blok-wynik");
                    })
         }
         }
-        
+//funkcja nie ma zastosowania 
         function sprawdzenieSeriiZnakow(tablicakrzyzykow, tablicakolek){
             
             
         }
-
+//funkcja sprawdzająca wygraną gracza i aktualizująca wynik(nie dziala)
         function sprawdzenieWygranej(){
             let tablicakrzyzykow = [];
             let tablicakolek = [];
@@ -132,7 +139,7 @@ const blokWynik = document.querySelector("#blok-wynik");
             }
            
         }
-        
+//funkcja postawienia figury na planszy(wyswietla odpowiedni obrazek w kratce,dodaje odpowiedni znak do klasy danej kratki i zmienia aktualnego gracza)
         function postawFigure(kratka){
            const kratki = document.querySelectorAll(".kratki"); 
            let figura = sprawdzenieAktualnejFigury();
